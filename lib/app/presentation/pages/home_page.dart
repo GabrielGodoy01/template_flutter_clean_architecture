23760b3be3bb2865cc302ae66d28682415a4510a
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-04-09 00:49:48 -0300

    without state management

--- lib/app/presentation/pages/home_page.dart
+++ lib/app/presentation/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_clean_architecture_template/app/domain/entities/user_entity.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,52 +15,6 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Users'),
       ),
       body: Container(),
-    );
-    // ListView.builder(
-    //   itemCount: 10,
-    //   itemBuilder: (context, index) {
-    //     final user = todoNotifier.users[index];
-    //     return ListTile(
-    //       leading: const Icon(Icons.person),
-    //       title: Text(
-    //         user.name,
-    //       ),
-    //       subtitle: Text('${user.id}'),
-    //       trailing: IconButton(
-    //         icon: const Icon(Icons.edit),
-    //         onPressed: () => editUserDialog(context, user),
-    //       ),
-    //     );
-    //   },
-    // ));
-  }
-
-  void editUserDialog(BuildContext context, UserEntity user) {
-    final TextEditingController _textEditingController =
-        TextEditingController();
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Edit user'),
-          content: TextFormField(
-            initialValue: user.name,
-            controller: _textEditingController,
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Save'),
-            ),
-          ],
-        );
-      },
     );
   }
 }
